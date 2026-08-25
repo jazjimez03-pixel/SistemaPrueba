1f36f5c449f48a660150425722345d67d6d56713
jazjimez03-pixel <jazjimez03@gmail.com>
2026-08-24 21:59:18 -0600

    Mi primer commit

---
+++ src/main/java/cr/ac/ucr/sistemaprueba/SistemaPrueba.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cr.ac.ucr.sistemaprueba;
+
+/**
+ *
+ * @author Jazmin
+ */
+public class SistemaPrueba {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
